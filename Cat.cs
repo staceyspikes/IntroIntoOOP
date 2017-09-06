51d2ba3978a22c7979062bdebafd1fa87f81147a
staceyspikesgit config --global user.name staceyspikes <stacey.lynne13@yahoo.com>
2017-09-06 19:35:29 -0400

    Practice

---
+++ Cat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroIntoOOP
+{
+    class Cat
+    {
+        private string name;
+        private string furColor;
+        private int furLength;
+        private int age;
+        private bool isMale;
+        private bool isHungry;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Age
+        {
+            get { return this.age; }
+        }
+        public Cat()
+        {
+            this.name = "Mittens";
+
+        }
+        public Cat(string name)
+        {
+            this.name = name;
+        }
+        
+        
+        public Cat(string name, string furColor, int furLength, int age, bool isMale)
+        {
+            this.name = name;
+            this.furColor = furColor;
+            this.furLength = furLength;
+            this.age = age;
+            this.isMale = isMale;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace IntroIntoOOP
     {
         static void Main(string[] args)
         {
+            Cat cat1 = new Cat();
+            Cat cat2 = new Cat("neko", "black", 3, 1, true);
+
+            Console.WriteLine(cat1.Name);
+            Console.WriteLine(cat2.Name);
+            cat1.Name = "gato";
+            Console.WriteLine(cat1.Name);
+            Console.WriteLine(cat2.Age);
+            Console.WriteLine(cat1.Age);
+            
         }
     }
 }
